d3269ae33de0a58a1753ff275e74f107ff4d6e37
Sean McNally <seanmcnally88@gmail.com>
2016-07-27 16:25:05 -0500

    Initial commit

---
+++ tour_nav_circle.js
@@ -0,0 +1,116 @@
+var one = document.querySelector('#one');
+var two = document.querySelector('#two');
+var three = document.querySelector('#three');
+var four = document.querySelector('#four');
+var five = document.querySelector('#five');
+var six = document.querySelector('#six');
+var seven = document.querySelector('#seven');
+var eight = document.querySelector('#eight');
+var nine = document.querySelector('#nine');
+
+document.querySelector("#demo_nav_1").addEventListener("click", function() {
+  one.setAttribute("visible", true);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_2").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", true);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_3").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", true);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_4").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", true);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_5").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", true);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_6").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", true);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_7").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", true);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", false);
+});
+
+document.querySelector("#demo_nav_8").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", true);
+  nine.setAttribute("visible", false);
+});
+document.querySelector("#demo_nav_9").addEventListener("click", function() {
+  one.setAttribute("visible", false);
+  two.setAttribute("visible", false);
+  three.setAttribute("visible", false);
+  four.setAttribute("visible", false);
+  five.setAttribute("visible", false);
+  six.setAttribute("visible", false);
+  seven.setAttribute("visible", false);
+  eight.setAttribute("visible", false);
+  nine.setAttribute("visible", true);
+});
